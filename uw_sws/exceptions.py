83d7bbdf7039096674fa821c94c5338fa3e24792
Patrick Michaud <pmichaud@uw.edu>
2017-03-01 14:34:58 -0800

    some basic moves of models, exceptions, formatters

---
+++ uw_sws/exceptions.py
@@ -0,0 +1,8 @@
+class InvalidCanvasIndependentStudyCourse(Exception):
+    """Exception for invalid Canvas course."""
+    pass
+
+
+class InvalidCanvasSection(Exception):
+    """Exception for invalid Canvas section."""
+    pass
