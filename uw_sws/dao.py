e0823e3bd91357f1a887e6f108c69c68ffbe5665
Jim Laney <jlaney@uw.edu>
2017-04-20 10:01:24 -0700

    moves graderoster to its own app

--- uw_sws/dao.py
+++ uw_sws/dao.py
@@ -37,64 +37,7 @@ class SWS_DAO(DAO):
             self._update_put(url, body, response)
 
     def _update_put(self, url, body, response):
-        # For developing against crashes in grade submission
-        if re.match('/student/v\d/graderoster/2013,spring,ZERROR,101,S1,',
-                    url):
-            response.data = "No employee found for ID 1234567890"
-            response.status = 500
-
-        # Submitted too late, sad.
-        if re.match('/student/v\d/graderoster/2013,spring,ZERROR,101,S2,',
-                    url):
-            response.data = "grading period not active for year/quarter"
-            response.status = 404
-
-        if body is not None:
-            response.status = 200
-            response.headers = {"X-Data-Source": "SWS file mock data"}
-            response.data = self._make_grade_roster_submitted(body)
-        else:
-            response.status = 400
-            response.data = "Bad Request: no PUT body"
-
-    def _make_grade_roster_submitted(self, submitted_body):
-        root = etree.fromstring(submitted_body)
-        item_elements = root.findall('.//*[@class="graderoster_item"]')
-        for item in item_elements:
-            date_graded = item.find('.//*[@class="date_graded date"]')
-            if date_graded.text is None:
-                date_graded.text = '2013-06-01'
-
-            grade_submitter_source = item.find(
-                './/*[@class="grade_submitter_source"]')
-            if grade_submitter_source.text is None:
-                grade_submitter_source.text = 'WEBCGB'
-
-            # Set the status code and message for each item, these elements
-            # aren't present in graderosters returned from GET
-            # Use settings.GRADEROSTER_PARTIAL_SUBMISSIONS to simulate failures
-            status_code_text = '200'
-            status_message_text = ''
-            if (getattr(settings,
-                        'GRADEROSTER_PARTIAL_SUBMISSIONS',
-                        False) and random.choice([True, False])):
-
-                status_code_text = '500'
-                status_message_text = 'Invalid grade'
-
-            status_code = item.find('.//*[@class="code"]')
-            if status_code is None:
-                status_code = etree.fromstring('<span class="code"/>')
-                item.append(status_code)
-            status_code.text = status_code_text
-
-            status_message = item.find('.//*[@class="message"]')
-            if status_message is None:
-                status_message = etree.fromstring('<span class="message"/>')
-                item.append(status_message)
-            status_message.text = status_message_text
-
-        return etree.tostring(root)
+        pass
 
     def _update_get(self, url, response):
         if "/student/v5/notice" in url:
