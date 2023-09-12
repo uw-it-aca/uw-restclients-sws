ab466a4b6084baba84c4f706451e3dbff91d7b6f
Stephen De Vight <devights@gmail.com>
2023-09-12 16:05:34 -0700

    adding pending resident change field to enrollment resource

--- uw_sws/models.py
+++ uw_sws/models.py
@@ -1373,6 +1373,7 @@ class Enrollment(models.Model):
 
     is_registered = models.NullBooleanField()
     has_pending_major_change = models.NullBooleanField()
+    has_pending_resident_change = models.NullBooleanField()
 
     def __init__(self, *args, **kwargs):
         self.registrations = []
@@ -1399,6 +1400,8 @@ class Enrollment(models.Model):
             'PendingMajorChange', False)
         self.is_enroll_src_pce = self._is_src_location_pce(
             json_data.get('Metadata', ''), ENROLLMENT_SOURCE_PCE)
+        self.has_pending_resident_change = json_data.get(
+            'PendingResidentChange', False)
 
         self.term = kwargs.get("term")
 
@@ -1452,6 +1455,7 @@ class Enrollment(models.Model):
             'is_enroll_src_pce': self.is_enroll_src_pce,
             'is_registered': self.is_registered,
             'has_pending_major_change': self.has_pending_major_change,
+            'has_pending_resident_change': self.has_pending_resident_change,
             'registrations': [r.json_data(
                 include_section_ref=True) for r in self.registrations],
             'majors': [m.json_data() for m in self.majors],
