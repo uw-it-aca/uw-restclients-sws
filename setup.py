1509a1809b3ba8cd770762264cbe3fd07d689608
Jim Laney <jlaney@uw.edu>
2024-02-03 08:44:47 -0800

    fix required installs

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
     include_package_data=True,
     install_requires=['UW-RestClients-Core',
                       'UW-RestClients-PWS',
-                      'zoneinfo',
                       'mock',
                      ],
     license='Apache License, Version 2.0',
