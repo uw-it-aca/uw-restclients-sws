dfcdcfb4d8b1a5bd38e8bf982073582feac9f646
Jim Laney <jlaney@uw.edu>
2024-02-21 09:45:10 -0800

    Use pytz for sws tz

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     include_package_data=True,
     install_requires=['UW-RestClients-Core',
                       'UW-RestClients-PWS',
+                      'pytz',
                       'mock',
                      ],
     license='Apache License, Version 2.0',
